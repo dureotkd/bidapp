8808bf73af3efe53b8a7903c9c5c44fffc3bf859
성민 <63378895+dureotkd@users.noreply.github.com>
2024-01-26 15:15:48 +0900

    hello

--- public/electron.js
+++ public/electron.js
@@ -1,11 +1,13 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+
+const fs = require("fs").promises;
+
+const path = require("path");
 
 // ! isDev [commonjs module 사용으로 인한 오류]
 // const isDev = require("electron-is-dev");
 
 const isDev = true;
-
-const path = require("path");
 
 let mainWindow = null;
 
@@ -16,6 +18,8 @@ const createWindow = () => {
     webPreferences: {
       nodeIntegration: true,
       devTools: isDev,
+      // * TypeError: window.require is not a function 일때 추가
+      contextIsolation: false,
     },
   });
 
@@ -36,7 +40,7 @@ const createWindow = () => {
 };
 
 // electron이 초기화 끝났을 때
-app.on("ready", () => {
+app.on("ready", async () => {
   createWindow();
 });
 
@@ -52,4 +56,202 @@ app.on("activate", () => {
   if (!mainWindow) {
     createWindow();
   }
+
+  console.log("activate");
 });
+
+const directoriesToSearch = ["C:\\", "D:\\"];
+
+ipcMain.on("GET_BID_FILE", async (event, arg) => {
+  console.log(
+    `============================== Start ==============================`
+  );
+
+  /**
+   * Electron 메인 프로세스에서 시간이 오래걸리는 작업을 수행할떄는
+   * 렌더러 프로세스(화면)를 차단하지않도록 비동기적으로 처리!!
+   */
+  const [bidFiles1, bidFiles2] = await Promise.all(
+    directoriesToSearch.map(async (directory) => {
+      return await walkDir(directory);
+    })
+  );
+
+  const bidFiles = [...bidFiles1, ...bidFiles2];
+
+  console.log(bidFiles);
+
+  if (!empty(bidFiles1)) {
+    for (const { file, filePath } of bidFiles) {
+      console.log(`file ${file} exists`);
+    }
+  }
+
+  console.log(
+    `============================== End ==============================`
+  );
+
+  mainWindow.webContents.send("GET_BID_FILE", { bidFiles1, bidFiles2 });
+});
+
+// 재귀적으로 디렉토리 탐색하는 함수
+async function walkDir(dir) {
+  try {
+    const files = await fs.readdir(dir, { withFileTypes: true });
+
+    const bidFiles = [];
+
+    for (const { name: file } of files) {
+      if (ignoreFolders(file)) {
+        try {
+          const filePath = path.join(dir, file);
+          const stat = await fs.stat(filePath);
+
+          if (stat.isDirectory()) {
+            // 디렉토리인 경우 재귀적으로 탐색
+            await walkDir(filePath);
+          } else {
+            if (file.endsWith(".BID") || file.endsWith(".bid")) {
+              // 파일인 경우 배열에 추가
+              bidFiles.push({
+                file,
+                filePath,
+              });
+            }
+          }
+        } catch (e) {
+          // 오류 발생 시 처리
+          // console.error(e);
+        }
+      }
+    }
+
+    return bidFiles;
+  } catch (e) {
+    // 오류 발생 시 처리
+    // console.error(`Error reading directory ${e.message}`);
+    return [];
+  }
+}
+
+function ignoreFolders(file) {
+  let isContinue = true;
+
+  if (file.includes("MSOCache")) {
+    isContinue = false;
+  }
+
+  if (file.includes("Recovery")) {
+    isContinue = false;
+  }
+
+  if (file.includes("System")) {
+    isContinue = false;
+  }
+
+  if (file.includes("node_modules")) {
+    isContinue = false;
+  }
+
+  if (file.includes("svn")) {
+    isContinue = false;
+  }
+
+  if (file.includes("git")) {
+    isContinue = false;
+  }
+
+  if (file.includes("php")) {
+    isContinue = false;
+  }
+
+  if (file.includes("svg")) {
+    isContinue = false;
+  }
+
+  if (file.includes("Program Files")) {
+    isContinue = false;
+  }
+
+  if (file.includes("PerfLogs")) {
+    isContinue = false;
+  }
+
+  if (file.includes("ProgramData")) {
+    isContinue = false;
+  }
+
+  if (file.includes("Windows")) {
+    isContinue = false;
+  }
+
+  if (file.includes("$")) {
+    isContinue = false;
+  }
+
+  if (file.includes(".log")) {
+    isContinue = false;
+  }
+
+  if (file.includes(".sys")) {
+    isContinue = false;
+  }
+
+  if (file.includes("OneDrive")) {
+    isContinue = false;
+  }
+  if (file.includes("workSpace")) {
+    isContinue = false;
+  }
+
+  if (file.includes("AppData")) {
+    isContinue = false;
+  }
+
+  if (file.includes(".metadata")) {
+    isContinue = false;
+  }
+
+  if (file.includes(".php")) {
+    isContinue = false;
+  }
+  if (file.includes(".css")) {
+    isContinue = false;
+  }
+  if (file.includes(".js")) {
+    isContinue = false;
+  }
+
+  if (file.includes("gradle")) {
+    isContinue = false;
+  }
+  if (file.includes("space")) {
+    isContinue = false;
+  }
+
+  return isContinue;
+}
+
+function empty(value) {
+  if (value === undefined || value === null) {
+    return true;
+  }
+
+  if (typeof value === "string" && value.trim() === "") {
+    return true;
+  }
+
+  if (typeof value === "number" && value === 0) {
+    return true;
+  }
+
+  if (Array.isArray(value) && value.length === 0) {
+    return true;
+  }
+
+  if (typeof value === "object" && Object.keys(value).length === 0) {
+    return true;
+  }
+
+  return false;
+}
--- src/App.js
+++ src/App.js
@@ -1,7 +1,66 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import axios from "axios";
+const { ipcRenderer } = window.require("electron");
 
+/**
+ * = 비드파일확인 프로그램
+ *  1. 내 PC에 있는 비드파일을 전부 보여준다
+ *  [공고명 검색 OR 공고번호 검색]
+ *
+ *  [정렬 우선순위]
+ *  - 다운로드 순서
+ *  - 투찰마감 순서
+ *  - 공고별로 분류
+ *
+ *  [화면에 표시되는 컬럼]
+ *  - 공고명 [공고번호]
+ *  - 사업자번호
+ *  - 투찰금액
+ *  - 입찰일
+ *  - 투찰마감일
+ *  - 다운로드 일자
+ *  - 투찰완료클릭
+ *  - 공고상세 링크
+ *
+ *
+ *
+ * @returns
+ */
 function App() {
+  const [loading, setLoading] = React.useState(true);
+  const [bidFiles, setBidFiles] = React.useState([]);
+
+  React.useEffect(() => {
+    const handleBidFile = (event, bidFIles) => {
+      setBidFiles(bidFIles);
+      setLoading(false);
+    };
+
+    ipcRenderer.on("GET_BID_FILE", handleBidFile);
+
+    return () => {
+      ipcRenderer.removeListener("GET_BID_FILE", handleBidFile);
+    };
+
+    // 컴포넌트가 처음 마운트될 때 한 번만 실행되도록 빈 배열을 두 번째 인자로 전달
+  }, []);
+
+  React.useEffect(() => {
+    // 이 부분에서 필요한 로직을 수행
+    (async () => {
+      await new Promise((resolve, reject) => {
+        ipcRenderer.send("GET_BID_FILE", "send");
+        resolve();
+      });
+
+      console.log("hello");
+    })();
+  }, []);
+
+  console.log(bidFiles);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +74,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactasd
+          Learn
+          {loading ? "Loading..." : "End"}
         </a>
       </header>
     </div>
